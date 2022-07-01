52432b24da364f3f74fbb14d315c9f86d607b962
Donniecia  Corke <donnieciamc@gmail.com>
2022-06-30 22:42:25 -0400

    addition of var and functions in java script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // Assignment Codes
 var generateBtn = document.querySelector("#generate");
+//Special Characters
 var specialCharacters = [
   "~",
   "`",
@@ -32,7 +33,9 @@ var specialCharacters = [
   "?",
   "/",
 ];
+//number characters
 var numberCharacters = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+//lowercase characters
 var lowercaseCharacters = [
   "a",
   "b",
@@ -61,6 +64,7 @@ var lowercaseCharacters = [
   "y",
   "z",
 ];
+//uppercaseCharacters
 var uppercaseCharacters = [
   "A",
   "B",
@@ -97,52 +101,55 @@ var specialCheck;
 var numberCheck;
 var lowercaseCheck;
 var uppercaseCheck;
-
+//First prompt instructing password length 
 function determineLength() {
   passwordLength = prompt(
     "How many characters would you like to contain in your password?"
   );
   if (passwordLength < 8) {
+    //if user enters characters less than 8 alerted that it needs to be at least 8 characters
     alert("Password must be at least 8 characters and no more than 129 characters.");
     determineLength();
+    //if user enters characters greater than 129 alerted that it needs to be at less than 129 characters
   } else if (passwordLength > 129) {
     alert("Password must be less than 129 characters.");
     determineLength();
+    //if user enters anything but a number, then alerted to enter a number 
   } else if (isNaN(passwordLength)) {
-    alert("Password length must be a number.");
+    alert("Please enter a number only.");
     determineLength();
   } else {
     alert(
-      "Choose what types of characters would you like to use in your Generated Password."
+      "Choose what types of characters would you like to use in your generated password."
     );
   }
   return passwordLength;
 }
-
+//Prompt instructing special character 
 function determineSpecial() {
   specialCheck = confirm("Click OK to comfirm inclusion of special characters.");
   return specialCheck;
 }
-
+//Prompt instructing number characters
 function determineNumber() {
   numberCheck = confirm("Click OK to comfirm inclusion of number characters.");
   return numberCheck;
 }
-
+//Prompt instructing lowercase characters
 function determinelowercase() {
   lowercaseCheck = confirm(
     "Click OK to comfirm inclusion of lowercase characters."
   );
   return lowercaseCheck;
 }
-
+//Prompt instructing uppercase characters
 function determineuppercase() {
   uppercaseCheck = confirm(
     "Click OK to comfirm inclusion of uppercase characters."
   );
   return uppercaseCheck;
 }
-
+//Addition of the console for each of the password criteria
 function generatePassword() {
   determineLength();
   console.log(passwordLength);
@@ -180,25 +187,13 @@ function generatePassword() {
     returnedPassword =
       returnedPassword +
       passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-    // console.log(tryrunningPassword);
+    // console.log(returnedPassword);
   }
 
   console.log(returnedPassword);
   return returnedPassword;
 }
 
-// function generatePassword() {
-//   determineLength();
-//   console.log(passwordLength);
-//   determineSpecial();
-//   console.log(specialCheck);
-//   determineNumber();
-//   console.log(numberCheck);
-//   determinelowercase();
-//   console.log(lowercaseCheck);
-//   determineuppercase();
-//   console.log(uppercaseCheck);
-// }
 
 // Write password to the #password input
 function writePassword() {
@@ -207,10 +202,6 @@ function writePassword() {
   passwordText.value = passwordLength;
 }
 
-// function resetText() {
-//   document.getElementById("password").value = "Your Secure Password";
-// }
-
-// Add event listener to generate button
+// Add event listener to generate button on click
 generateBtn.addEventListener("click", writePassword);
 
